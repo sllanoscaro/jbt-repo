4a79daa33fa56135bb2d769c087cd93c5a30d643
oruava <a.avalos02@ufromail.cl>
2023-09-06 21:20:14 -0300

    segundo commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[]args){
         System.out.println("Hola Mundo");
-        System.out.println("Arturo Avalos");
+        System.out.println("Arturo Avalos Pascual");
     }
 
 }
